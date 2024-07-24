8f651ba2fee18ce88736a28a9f6558f66f73f5b6
Jacob Pratt <jacob@jhpratt.dev>
2024-07-24 16:20:57 -0400

    Add ability to delete blob, stub Rust API

--- build.rs
+++ build.rs
@@ -1,7 +1,14 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-   tonic_build::configure()
+    tonic_build::configure()
         .build_server(true)
-        .compile(&["src/store/kv/kv_schema.proto","src/store/stream/stream_schema.proto"], &["src/store/kv", "src/store/stream"])
+        .compile(
+            &[
+                "src/store/kv/kv_schema.proto",
+                "src/store/stream/stream_schema.proto",
+                "src/store/blob/blob_schema.proto",
+            ],
+            &["src/store/kv", "src/store/stream", "src/store/blob"],
+        )
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
     Ok(())
 }
