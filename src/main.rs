5186c83c1368530ff24ecb8302ea096d52620b99
Jacob Pratt <jacob@jhpratt.dev>
2024-07-25 01:42:42 -0400

    Add command line arguments

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,38 @@
 
 pub mod store;
 
+use std::net::SocketAddr;
+use std::path::PathBuf;
+
+use clap::Parser;
 use rusqlite::{Connection, Error as SqliteError, Result as SqliteResult};
 use tonic::transport::Server;
 
 type RpcResponse<T> = Result<tonic::Response<T>, tonic::Status>;
 
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Opts {
+    /// The file to store key-value pairs in.
+    #[clap(long, default_value = "kv_store.sqlite3")]
+    kv_store: PathBuf,
+    /// The file to store blobs in.
+    #[clap(long, default_value = "blob_store.sqlite3")]
+    blob_store: PathBuf,
+    #[clap(long, default_value = "[::1]:50051")]
+    addr: SocketAddr,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+    let Opts {
+        kv_store,
+        blob_store,
+        addr,
+    } = Opts::parse();
 
-    let kv_store = store::kv::KvStore::new("kv_store.sqlite3");
-    let blob_store = store::blob::BlobStore::new("blob_store.sqlite3");
+    let kv_store = store::kv::KvStore::new(kv_store);
+    let blob_store = store::blob::BlobStore::new(blob_store);
 
     Server::builder()
         .add_service(store::kv::KeyValueServer::new(kv_store))
