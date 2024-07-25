efce78215d5adbccb1121294f755a8fd930e5880
Jacob Pratt <jacob@jhpratt.dev>
2024-07-24 21:24:03 -0400

    Add tests for KvStore

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ pub mod store;
 
 use rusqlite::{Connection, Error as SqliteError, Result as SqliteResult};
 
+type RpcResponse<T> = Result<tonic::Response<T>, tonic::Status>;
+
 fn main() -> SqliteResult<()> {
     // Connect to or create a new database
     let _conn = Connection::open("db.sqlite3")?;
--- src/store/kv/mod.rs
+++ src/store/kv/mod.rs
@@ -2,14 +2,14 @@ mod kv_store {
     tonic::include_proto!("kvstore");
 }
 
-use std::collections::HashMap;
-use std::path::PathBuf;
-
 use crate::store;
 pub use crate::store::kv::kv_store::key_value_server::KeyValue;
 pub use crate::store::kv::kv_store::{
     DeleteRequest, DeleteResponse, GetRequest, GetResponse, SetRequest, SetResponse,
 };
+use crate::RpcResponse;
+use std::collections::HashMap;
+use std::path::PathBuf;
 use tonic::{Request, Response, Status};
 
 #[derive(Debug, PartialEq, Eq)]
@@ -42,7 +42,7 @@ impl KvStore {
                 .or_insert_with(|| self.location.to_connection());
 
             db.execute(
-                "CREATE TABLE IF NOT EXISTS kv (
+                "CREATE TABLE IF NOT EXISTS kv_store (
                     key TEXT PRIMARY KEY,
                     value TEXT NOT NULL
                 )",
@@ -56,10 +56,10 @@ impl KvStore {
 
 #[tonic::async_trait]
 impl KeyValue for KvStore {
-    async fn get(&self, request: Request<GetRequest>) -> Result<Response<GetResponse>, Status> {
+    async fn get(&self, request: Request<GetRequest>) -> RpcResponse<GetResponse> {
         self.with_db(|db| {
             let value = db.query_row(
-                "SELECT value FROM kv WHERE key = ?1",
+                "SELECT value FROM kv_store WHERE key = ?1",
                 [&request.get_ref().key],
                 |row| row.get(0),
             );
@@ -72,11 +72,11 @@ impl KeyValue for KvStore {
         })
     }
 
-    async fn set(&self, request: Request<SetRequest>) -> Result<Response<SetResponse>, Status> {
+    async fn set(&self, request: Request<SetRequest>) -> RpcResponse<SetResponse> {
         let request = request.get_ref();
         self.with_db(|db| {
             let value = db.execute(
-                "REPLACE INTO kv (key, value) VALUES (?1, ?2)",
+                "REPLACE INTO kv_store (key, value) VALUES (?1, ?2)",
                 [&request.key, &request.value],
             );
 
@@ -90,13 +90,10 @@ impl KeyValue for KvStore {
         })
     }
 
-    async fn delete(
-        &self,
-        request: Request<DeleteRequest>,
-    ) -> Result<Response<DeleteResponse>, Status> {
+    async fn delete(&self, request: Request<DeleteRequest>) -> RpcResponse<DeleteResponse> {
         self.with_db(|db| {
             let value = db.query_row(
-                "DELETE FROM kv WHERE key = ?1 RETURNING value",
+                "DELETE FROM kv_store WHERE key = ?1 RETURNING value",
                 [&request.get_ref().key],
                 |row| row.get(0),
             );
@@ -107,5 +104,74 @@ impl KeyValue for KvStore {
                 Err(_) => Err(Status::new(tonic::Code::NotFound, "key not found")),
             }
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get() -> Result<(), Box<dyn std::error::Error>> {
+        let store = KvStore::new_in_memory();
+
+        store
+            .set(Request::new(SetRequest {
+                key: "key".to_owned(),
+                value: "value".to_owned(),
+            }))
+            .await?;
+
+        let response = store
+            .get(Request::new(GetRequest {
+                key: "key".to_owned(),
+            }))
+            .await?;
+        assert_eq!(response.get_ref().value, "value");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_set() -> Result<(), Box<dyn std::error::Error>> {
+        let store = KvStore::new_in_memory();
+
+        let response = store
+            .set(Request::new(SetRequest {
+                key: "key".to_owned(),
+                value: "value".to_owned(),
+            }))
+            .await?;
+        assert_eq!(response.get_ref(), &SetResponse {});
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_delete() -> Result<(), Box<dyn std::error::Error>> {
+        let store = KvStore::new_in_memory();
+
+        store
+            .set(Request::new(SetRequest {
+                key: "key".to_owned(),
+                value: "value".to_owned(),
+            }))
+            .await?;
+
+        let response = store
+            .delete(Request::new(DeleteRequest {
+                key: "key".to_owned(),
+            }))
+            .await?;
+        assert_eq!(response.get_ref().old_value, "value");
+
+        let response = store
+            .get(Request::new(GetRequest {
+                key: "key".to_owned(),
+            }))
+            .await;
+        assert!(response.is_err());
+
+        Ok(())
     }
 }
