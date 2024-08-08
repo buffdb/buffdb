e57a9ae5bb71f907b59a87fedad0da5fea768d71
Jacob Pratt <jacob@jhpratt.dev>
2024-08-07 22:00:41 -0400

    Benchmark fetching data from database

--- benches/kv.rs
+++ benches/kv.rs
@@ -5,7 +5,9 @@
     clippy::unwrap_used
 )]
 
-use buffdb::proto::kv::SetRequest;
+use buffdb::client::kv::KvClient;
+use buffdb::client::query::QueryClient;
+use buffdb::proto::kv::{GetRequest, SetRequest};
 use buffdb::proto::query::{RawQuery, TargetStore};
 use buffdb::{backend, transitive, Location};
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
@@ -15,7 +17,75 @@ use std::iter;
 
 // TODO randomize the length of generated strings
 
-const QUERIES_PER_BATCH: usize = 1_000;
+const INSERT_QUERIES_PER_BATCH: usize = 1_000;
+const GET_QUERIES_PER_BATCH: usize = 1_000;
+
+/// Insert one million rows into the database, returning `RETURN_COUNT` keys. All other
+/// keys are effectively discarded, only being present in the database to ensure a real-world load.
+async fn insert_1m_rows_raw<T, const RETURN_COUNT: usize>(conn: &mut QueryClient<T>) -> Vec<String>
+where
+    T: tonic::client::GrpcService<tonic::body::BoxBody, Future: Send> + Send,
+    T::Error: Into<Box<dyn std::error::Error + Send + Sync + 'static>>,
+    T::ResponseBody: tonic::transport::Body<Data = prost::bytes::Bytes> + Send + 'static,
+    <T::ResponseBody as tonic::transport::Body>::Error:
+        Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
+{
+    let mut rng = rand::rngs::OsRng;
+    let requests: Vec<_> = iter::from_fn(|| {
+        let key = Alphanumeric.sample_string(&mut rng, 20);
+        let value = Alphanumeric.sample_string(&mut rng, 20);
+        Some((
+            RawQuery {
+                query: format!("INSERT INTO kv (key, value) VALUES ('{key}', '{value}')"),
+                target: TargetStore::Kv as _,
+            },
+            key,
+        ))
+    })
+    .take(1_000_000)
+    .collect();
+
+    let ret = requests
+        .iter()
+        .map(|req| req.1.clone())
+        .take(RETURN_COUNT)
+        .collect();
+
+    let requests = requests.into_iter().map(|(req, _)| req).collect::<Vec<_>>();
+    conn.execute(stream::iter(requests)).await.unwrap();
+
+    ret
+}
+
+/// Insert one million rows into the database, returning `RETURN_COUNT` keys. All other
+/// keys are effectively discarded, only being present in the database to ensure a real-world load.
+async fn insert_1m_rows<T, const RETURN_COUNT: usize>(conn: &mut KvClient<T>) -> Vec<String>
+where
+    T: tonic::client::GrpcService<tonic::body::BoxBody, Future: Send> + Send,
+    T::Error: Into<Box<dyn std::error::Error + Send + Sync + 'static>>,
+    T::ResponseBody: tonic::transport::Body<Data = prost::bytes::Bytes> + Send + 'static,
+    <T::ResponseBody as tonic::transport::Body>::Error:
+        Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
+{
+    let mut rng = rand::rngs::OsRng;
+    let requests: Vec<_> = iter::from_fn(|| {
+        let key = Alphanumeric.sample_string(&mut rng, 20);
+        let value = Alphanumeric.sample_string(&mut rng, 20);
+        Some(SetRequest { key, value })
+    })
+    .take(1_000_000)
+    .collect();
+
+    let ret = requests
+        .iter()
+        .map(|req| req.key.clone())
+        .take(RETURN_COUNT)
+        .collect();
+
+    conn.set(stream::iter(requests)).await.unwrap();
+
+    ret
+}
 
 fn sqlite_insert_raw(c: &mut Criterion) {
     let runtime = tokio::runtime::Builder::new_multi_thread()
@@ -54,7 +124,7 @@ fn sqlite_insert_raw(c: &mut Criterion) {
                         target: TargetStore::Kv as _,
                     })
                 })
-                .take(QUERIES_PER_BATCH)
+                .take(INSERT_QUERIES_PER_BATCH)
                 .collect();
                 (stream::iter(queries), client.clone())
             },
@@ -91,7 +161,7 @@ fn sqlite_insert(c: &mut Criterion) {
                     let value = Alphanumeric.sample_string(&mut rng, 20);
                     Some(SetRequest { key, value })
                 })
-                .take(QUERIES_PER_BATCH)
+                .take(INSERT_QUERIES_PER_BATCH)
                 .collect();
                 (stream::iter(requests), client.clone())
             },
@@ -103,5 +173,75 @@ fn sqlite_insert(c: &mut Criterion) {
     });
 }
 
-criterion_group!(buffdb, sqlite_insert, sqlite_insert_raw);
+fn sqlite_get_raw(c: &mut Criterion) {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(1)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let (client, keys) = runtime.block_on(async {
+        let mut client =
+            transitive::query_client::<_, _, backend::Sqlite>(Location::InMemory, "/dev/null")
+                .await
+                .unwrap();
+        let keys = insert_1m_rows_raw::<_, GET_QUERIES_PER_BATCH>(&mut client).await;
+        (client, keys)
+    });
+
+    let queries = keys
+        .into_iter()
+        .map(|key| RawQuery {
+            query: format!("SELECT (key, value) FROM kv WHERE key = '{key}'"),
+            target: TargetStore::Kv as _,
+        })
+        .collect::<Vec<_>>();
+
+    c.bench_function("sqlite_kv_get_raw", |b| {
+        b.to_async(&runtime).iter_batched(
+            || (stream::iter(queries.clone()), client.clone()),
+            |(queries, mut client)| async move { client.query(queries).await.unwrap() },
+            BatchSize::SmallInput,
+        );
+    });
+}
+
+fn sqlite_get(c: &mut Criterion) {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(1)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let (client, keys) = runtime.block_on(async {
+        let mut client = transitive::kv_client::<_, backend::Sqlite>(Location::InMemory)
+            .await
+            .unwrap();
+        let keys = insert_1m_rows::<_, GET_QUERIES_PER_BATCH>(&mut client).await;
+        (client, keys)
+    });
+
+    let keys = keys
+        .into_iter()
+        .map(|key| GetRequest { key })
+        .collect::<Vec<_>>();
+
+    c.bench_function("sqlite_kv_get", |b| {
+        b.to_async(&runtime).iter_batched(
+            || (stream::iter(keys.clone()), client.clone()),
+            |(keys, mut client)| async move {
+                client.get(keys).await.unwrap();
+            },
+            BatchSize::SmallInput,
+        );
+    });
+}
+
+criterion_group!(
+    buffdb,
+    sqlite_insert,
+    sqlite_insert_raw,
+    sqlite_get,
+    sqlite_get_raw
+);
 criterion_main!(buffdb);
